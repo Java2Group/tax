cd852d1ffa228f1d9986b9198ea030a58acf99ea
Brandon Yaeck <yaeckb@sheridancollege.ca>
2023-03-17 10:37:49 -0400

    updated login page

--- src/tax/Login.java
+++ src/tax/Login.java
@@ -3,9 +3,12 @@ package tax;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ContentDisplay;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
@@ -21,36 +24,38 @@ public class Login extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-
 
 	Label emailLabel = new Label("Email Address");
+	HBox emailLabelHbox = new HBox();
+	emailLabelHbox.getChildren().add(emailLabel);
+
 	TextField emailField = new TextField();
 
 	Label passwordLabel = new Label("Password");
-	TextField passwordField = new TextField();
+	HBox passwordLabelHbox = new HBox();
+	passwordLabelHbox.getChildren().add(passwordLabel);
+
+	PasswordField passwordField = new PasswordField();
 
 	Button loginButton = new Button("Log In");
 
         loginButton.setOnAction(e -> {
-		
+		System.out.println(emailField.getText());
+		System.out.println(passwordField.getText());
 	});
 
-	Text registerLink = new Text("Don't have an account? Click here to register.");
+	Button registerLink = new Button("Don't have an account? Click here to register.");
+	registerLink.setStyle("-fx-background-color: rgba(0,0,0,0)");
 
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
+        registerLink.setOnAction(e -> {
+		System.out.println("Test2");
+	});
+
 	VBox vb = new VBox();
-	vb.getChildren().addAll(emailLabel, emailField, passwordLabel, passwordField, loginButton, registerLink);
+	vb.getChildren().addAll(emailLabelHbox, emailField, passwordLabelHbox, passwordField, loginButton, registerLink);
+	vb.setAlignment(Pos.CENTER);
 
-        Scene scene = new Scene(vb, 300, 250);
+        Scene scene = new Scene(vb, 500, 400);
 
         primaryStage.setTitle("Tax Login");
         primaryStage.setScene(scene);
