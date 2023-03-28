d76108814d78d62030c5ee63dbe40530af7632ac
Brandon Yaeck <yaeckb@sheridancollege.ca>
2023-03-27 22:39:37 -0400

    updated login page structure

--- src/tax/Login.java
+++ src/tax/Login.java
@@ -3,6 +3,7 @@ package tax;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -26,16 +27,20 @@ public class Login extends Application {
     public void start(Stage primaryStage) {
 
 	Label emailLabel = new Label("Email Address");
-	HBox emailLabelHbox = new HBox();
-	emailLabelHbox.getChildren().add(emailLabel);
-
 	TextField emailField = new TextField();
 
-	Label passwordLabel = new Label("Password");
-	HBox passwordLabelHbox = new HBox();
-	passwordLabelHbox.getChildren().add(passwordLabel);
+	VBox emailBox = new VBox();
+	emailBox.getChildren().addAll(emailLabel, emailField);
+	emailBox.setPrefWidth(200);
 
+
+	Label passwordLabel = new Label("Password");
 	PasswordField passwordField = new PasswordField();
+
+	VBox passwordBox = new VBox();
+	passwordBox.getChildren().addAll(passwordLabel, passwordField);
+	passwordBox.setPrefWidth(200);
+
 
 	Button loginButton = new Button("Log In");
 
@@ -44,18 +49,27 @@ public class Login extends Application {
 		System.out.println(passwordField.getText());
 	});
 
+
 	Button registerLink = new Button("Don't have an account? Click here to register.");
 	registerLink.setStyle("-fx-background-color: rgba(0,0,0,0)");
 
         registerLink.setOnAction(e -> {
-		System.out.println("Test2");
+		System.out.println("Go to registration");
 	});
 
-	VBox vb = new VBox();
-	vb.getChildren().addAll(emailLabelHbox, emailField, passwordLabelHbox, passwordField, loginButton, registerLink);
-	vb.setAlignment(Pos.CENTER);
 
-        Scene scene = new Scene(vb, 500, 400);
+	VBox submitPanel = new VBox(10);
+	submitPanel.setAlignment(Pos.CENTER);
+	submitPanel.getChildren().addAll(loginButton, registerLink);
+
+
+	VBox outerPanel = new VBox(20);
+	outerPanel.setAlignment(Pos.CENTER);
+	outerPanel.setFillWidth(false);
+	outerPanel.setPadding(new Insets(20, 20, 20, 20));
+	outerPanel.getChildren().addAll(emailBox, passwordBox, submitPanel);
+
+        Scene scene = new Scene(outerPanel);
 
         primaryStage.setTitle("Tax Login");
         primaryStage.setScene(scene);
