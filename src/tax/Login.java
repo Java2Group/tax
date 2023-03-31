50a980b5349015c0792b389a62d11ca5c3038f19
Brandon Yaeck <yaeckb@sheridancollege.ca>
2023-03-31 10:25:39 -0400

    fixed Login.java indentation

--- src/tax/Login.java
+++ src/tax/Login.java
@@ -16,61 +16,59 @@ import javafx.stage.Stage;
  */
 public class Login {
 
-public Scene getScene(Stage currentStage) {
-	Label emailLabel = new Label("Email Address");
-	TextField emailField = new TextField();
+	public Scene getScene(Stage currentStage) {
+		Label emailLabel = new Label("Email Address");
+		TextField emailField = new TextField();
 
-	VBox emailBox = new VBox();
-	emailBox.getChildren().addAll(emailLabel, emailField);
-	emailBox.setPrefWidth(200);
-
-
-	Label passwordLabel = new Label("Password");
-	PasswordField passwordField = new PasswordField();
-
-	VBox passwordBox = new VBox();
-	passwordBox.getChildren().addAll(passwordLabel, passwordField);
-	passwordBox.setPrefWidth(200);
+		VBox emailBox = new VBox();
+		emailBox.getChildren().addAll(emailLabel, emailField);
+		emailBox.setPrefWidth(200);
 
 
-	Button loginButton = new Button("Log In");
+		Label passwordLabel = new Label("Password");
+		PasswordField passwordField = new PasswordField();
 
-        loginButton.setOnAction(e -> {
-		System.out.println(emailField.getText());
-		System.out.println(passwordField.getText());
-	});
+		VBox passwordBox = new VBox();
+		passwordBox.getChildren().addAll(passwordLabel, passwordField);
+		passwordBox.setPrefWidth(200);
 
 
-	Button registerLink = new Button("Don't have an account? Click here to register.");
-	registerLink.setStyle("-fx-background-color: rgba(0,0,0,0); -fx-text-fill: rgba(0,0,0,100)");
+		Button loginButton = new Button("Log In");
 
-        registerLink.setOnAction(e -> {
-		
-		currentStage.setTitle("User Registration");
-		currentStage.setScene(new Registration().getScene(currentStage));
-	});
-	registerLink.setOnMouseEntered(e -> {
-		registerLink.setStyle("-fx-background-color: rgba(0,0,0,0); -fx-text-fill: rgba(0,0,255,100)");
-		registerLink.setCursor(Cursor.HAND);
-	});
-	registerLink.setOnMouseExited(e -> {
+		loginButton.setOnAction(e -> {
+			System.out.println(emailField.getText());
+			System.out.println(passwordField.getText());
+		});
+
+		Button registerLink = new Button("Don't have an account? Click here to register.");
 		registerLink.setStyle("-fx-background-color: rgba(0,0,0,0); -fx-text-fill: rgba(0,0,0,100)");
-	});
+
+		registerLink.setOnAction(e -> {
+			currentStage.setTitle("User Registration");
+			currentStage.setScene(new Registration().getScene(currentStage));
+		});
+		registerLink.setOnMouseEntered(e -> {
+			registerLink.setStyle("-fx-background-color: rgba(0,0,0,0); -fx-text-fill: rgba(0,0,255,100)");
+			registerLink.setCursor(Cursor.HAND);
+		});
+		registerLink.setOnMouseExited(e -> {
+			registerLink.setStyle("-fx-background-color: rgba(0,0,0,0); -fx-text-fill: rgba(0,0,0,100)");
+		});
 
 
-	VBox submitPanel = new VBox(10);
-	submitPanel.setAlignment(Pos.CENTER);
-	submitPanel.getChildren().addAll(loginButton, registerLink);
+		VBox submitPanel = new VBox(10);
+		submitPanel.setAlignment(Pos.CENTER);
+		submitPanel.getChildren().addAll(loginButton, registerLink);
 
 
-	VBox outerPanel = new VBox(20);
-	outerPanel.setAlignment(Pos.CENTER);
-	outerPanel.setFillWidth(false);
-	outerPanel.setPadding(new Insets(20, 20, 20, 20));
-	outerPanel.getChildren().addAll(emailBox, passwordBox, submitPanel);
+		VBox outerPanel = new VBox(20);
+		outerPanel.setAlignment(Pos.CENTER);
+		outerPanel.setFillWidth(false);
+		outerPanel.setPadding(new Insets(20, 20, 20, 20));
+		outerPanel.getChildren().addAll(emailBox, passwordBox, submitPanel);
 
-        Scene scene = new Scene(outerPanel);
+		Scene scene = new Scene(outerPanel);
 
-	return scene;
-    }
+		return scene;
+	}
 }
