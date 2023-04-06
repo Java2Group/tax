239d02aecfe3cd6ae8191df08a9d9032735b5f5a
Michael Sousa <sousa8@sheridancollege.ca>
2023-04-06 11:29:08 -0400

    Added file chooser to Results window.

--- src/tax/Results.java
+++ src/tax/Results.java
@@ -1,5 +1,6 @@
 package tax;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import javafx.application.Application;
@@ -13,6 +14,7 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.stage.FileChooser;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
@@ -30,31 +32,38 @@ public class Results extends Application {
         saveBox.setPadding(new Insets(40, 20, 20, 20));
         saveBox.setAlignment(Pos.CENTER);
         Label resultLabel = new Label(Calculate.getResult());
-        if(resultLabel.getText().contains("Owed")){
+        if (resultLabel.getText().contains("Owed")) {
             resultLabel.setTextFill(Color.RED);
-        }else{
+        } else {
             resultLabel.setTextFill(Color.GREEN);
         }
         HBox resultBox = new HBox(resultLabel);
-        resultBox.setPadding(new Insets(10,0,0,0));
+        resultBox.setPadding(new Insets(10, 0, 0, 0));
         resultBox.setAlignment(Pos.CENTER);
         VBox resultRoot = new VBox(resultBox, saveBox);
+        
+        //Creating a File chooser
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Save");
+        
+        //Saving file to location specified by the user in the .txt format.
         saveBtn.setOnAction(e -> {
-            try {
-
-                // Open a file named "tax_result.txt" in write mode
-                FileWriter writer = new FileWriter("taxresult.txt");
-                // Write the result string to the file
-                writer.write(Calculate.getResult());
-                // Close the file
-                writer.close();
-                resultLabel.setText("Result saved to file: taxresult.txt");
-            } catch (IOException exception) {
-                // Show an error message if there was an error writing to the file
-                Alert alert = new Alert(AlertType.ERROR);
-                alert.setTitle("Error");
-                alert.setHeaderText("Error saving result to file: " + exception.getMessage());
-                alert.showAndWait();
+            FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("Text Files (*.txt)", "*.txt");
+            fileChooser.getExtensionFilters().add(extFilter);
+            File file = fileChooser.showSaveDialog(resultStage);
+            if (file != null) {
+                try {
+                    FileWriter writer = new FileWriter(file);
+                    writer.write(Calculate.getResult()); 
+                    writer.close();
+                    resultLabel.setText("Results saved to file: " + file.getName());
+                } catch (IOException exception) {
+                    // Show an error message if there was an error writing to the file
+                    Alert alert = new Alert(AlertType.ERROR);
+                    alert.setTitle("Error");
+                    alert.setHeaderText("Error saving result to file: " + exception.getMessage());
+                    alert.showAndWait();
+                }
             }
         });
         Scene resultScene = new Scene(resultRoot, 500, 200);
